e2b69aecb05ebe5f951cdb8dfcf674e1f9c9c0be
Stefano <stefano.bragaglia@gmail.com>
2015-09-11 11:45:30 +0100

    Basic application structure

---
+++ src/main/java/app/Application.java
@@ -0,0 +1,10 @@
+package app;
+
+/**
+ * TODO Add some meaningful class description...
+ */
+public class Application {
+	public static void main(String[] args) {
+		System.out.println("Done.");
+	}
+}
